c01b291a9b7498f84440db0b31ae3e134fdbabdf
McoxfordG <mcoxford24@gmail.com>
2024-07-25 17:19:51 +0300

    work complete

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const taskInput = document.getElementById("task-input");
     const taskList = document.getElementById("task-list");
 
+    //Retrieving task list
+    function loadTask() {
+        const storedTask = JSON.parse(localStorage.getItem('tasks') || '[]');
+        storedTa.forEach(taskText => addTask(taskText, false));
+
+    }
+
     //Add Task function
-    function addTask() {
+    function addTask(taskText, save = true) {
         var taskText = taskInput.value.trim()
         const liElement = document.createElement("li");
 
@@ -21,10 +28,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
             removeButton.onclick = function() {
                 taskList.removeChild(liElement);
-            }
+
+        //Save 
+        if (save) {
+            const storedTask = JSON.parse(localStorage.getItem('tasks') || '[]');
+            storedTask = storedTask.filter(task => task !== taskText);
+            localStorage.setItem('tasks', JSON.stringify(storedTask));
+        }
+
+        };
             liElement.appendChild(removeButton);
             taskList.appendChild(liElement);
 
+            if (save) {
+                // Save the task to Local Storage
+                const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+                storedTasks.push(taskText);
+                localStorage.setItem('tasks', JSON.stringify(storedTasks));
+            }
+            
             taskInput.value = '';
         }
     }
@@ -35,4 +57,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
     document.addEventListener('DOMContentLoaded', addTask);
+
 });
